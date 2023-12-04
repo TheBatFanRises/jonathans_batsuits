d31bf91e9359dfd5809adec137b96e139f63f527
TheBatFanRises <143863636+TheBatFanRises@users.noreply.github.com>
2023-12-04 11:41:54 -0500

    Fixed compatibility issue with C2ME

--- src/main/java/net/jonathan/jonathansbatsuits/networking/ModPackets.java
+++ src/main/java/net/jonathan/jonathansbatsuits/networking/ModPackets.java
@@ -7,15 +7,13 @@ import net.jonathan.jonathansbatsuits.item.custom.BatmanBeyondItem;
 import net.jonathan.jonathansbatsuits.item.custom.ChristianBaleItem;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.EquipmentSlot;
-import net.minecraft.entity.SpawnReason;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.entity.passive.BatEntity;
 import net.minecraft.entity.projectile.FireworkRocketEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.Identifier;
-
-import java.util.Objects;
 
 public class ModPackets {
     public static final Identifier SUIT_ABILITY_ID = new Identifier(JonathansBatsuits.MOD_ID, "suit_ability");
@@ -23,10 +21,15 @@ public class ModPackets {
     public static void registerC2SPackets() {
         ServerPlayNetworking.registerGlobalReceiver(SUIT_ABILITY_ID, ((server, player, handler, buf, responseSender) -> {
 
+            //Because C2ME doesn't like the EntityType.spawn method, the ServerWorld.spawnEntity method needs to be called for sake of compatibility.
             if (ChristianBaleItem.isWearingAll(player)) {
                 for (int i = 0; i < 10; i++) {
-                    Objects.requireNonNull(EntityType.BAT.spawn((ServerWorld)player.getEntityWorld(), player.getBlockPos(), SpawnReason.TRIGGERED))
-                            .addStatusEffect(new StatusEffectInstance(StatusEffects.WITHER, -1));
+                    for (int i = 0; i <= 10; i++) {
+                    BatEntity batEntity = new BatEntity(EntityType.BAT, serverWorld);
+                    serverWorld.spawnEntity(batEntity);
+                    batEntity.addStatusEffect(new StatusEffectInstance(StatusEffects.WITHER, -1));
+                    batEntity.teleport(player.getX(), player.getY(), player.getZ());
+                }
                 }
             }
 
