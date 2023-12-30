5d4ff0f336e248e0671f904cff21708a56ccb0f6
TheBatFanRises <143863636+TheBatFanRises@users.noreply.github.com>
2023-12-30 09:16:21 -0500

    Fixed bat-glider interfering with vanilla elytra.

--- src/main/java/net/jonathan/jonathansbatsuits/mixin/RocketItemMixin.java
+++ src/main/java/net/jonathan/jonathansbatsuits/mixin/RocketItemMixin.java
@@ -1,11 +1,13 @@
 package net.jonathan.jonathansbatsuits.mixin;
 
+import net.jonathan.jonathansbatsuits.event.custom_events.ModElytraItem;
 import net.jonathan.jonathansbatsuits.integration.trinkets.ElytraTrinketHandler;
-import net.jonathan.jonathansbatsuits.item.ModItems;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.projectile.FireworkRocketEntity;
 import net.minecraft.item.FireworkRocketItem;
 import net.minecraft.item.ItemStack;
+import net.minecraft.stat.Stats;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.world.World;
@@ -20,17 +22,25 @@ import java.util.Objects;
 public class RocketItemMixin {
     @Inject(at = @At("HEAD"), method = "use", cancellable = true)
     protected void useExcludeBatGlider(World world, PlayerEntity user, Hand hand, CallbackInfoReturnable<TypedActionResult<ItemStack>> cir) {
-        ItemStack stack;
-        if (ElytraTrinketHandler.slotProvider == null) {
-            stack = user.getEquippedStack(EquipmentSlot.CHEST);
-        } else {
-            ElytraTrinketHandler.SlotProvider slotProvider = ElytraTrinketHandler.slotProvider;
-            Objects.requireNonNull(user);
-            stack = slotProvider.getElyra(user, user::getEquippedStack);
-        }
-        ItemStack itemStack = stack;
-        if (user.isFallFlying() && itemStack.getItem() == ModItems.BAT_GLIDER) {
+        
+        ItemStack itemStack = (ElytraTrinketHandler.slotProvider == null) ? user.getEquippedStack(EquipmentSlot.CHEST) :
+                ElytraTrinketHandler.slotProvider.getElytra(user, user::getEquippedStack);
+
+        if (itemStack != null && user.isFallFlying() && itemStack.getItem() instanceof ModElytraItem) {
             cir.setReturnValue(TypedActionResult.pass(user.getStackInHand(hand)));
+        }
+
+        if (itemStack == null && user.isFallFlying()) {
+            ItemStack itemStack1 = user.getStackInHand(hand);
+            if (!world.isClient) {
+                FireworkRocketEntity fireworkRocketEntity = new FireworkRocketEntity(world, itemStack1, user);
+                world.spawnEntity(fireworkRocketEntity);
+                if (!user.getAbilities().creativeMode) {
+                    itemStack1.decrement(1);
+                }
+                user.incrementStat(Stats.USED.getOrCreateStat(itemStack1.getItem()));
+            }
+            cir.setReturnValue(TypedActionResult.success(user.getStackInHand(hand), world.isClient()));
         }
     }
 }
